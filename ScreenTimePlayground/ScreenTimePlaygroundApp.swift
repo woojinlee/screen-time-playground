960abcbeb5a03c6e093f86e1d2d0deb665f8920f
Woojin <woojinlee87@gmail.com>
2024-03-14 11:39:33 +0900

    Create SwiftUI project

---
+++ ScreenTimePlayground/ScreenTimePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimePlaygroundApp.swift
+//  ScreenTimePlayground
+//
+//  Created by Woojin on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
